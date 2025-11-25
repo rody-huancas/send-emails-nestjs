0f1f8d5f655b3dd9a572f99db866b29c6be76acd
Rody Huancas <rodyhuancas.04@gmail.com>
2025-11-24 23:01:08 -0500

    Feat: Enhance bootstrap function with global prefix and validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from '@config/envs';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap'); 
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api', {
+    exclude: [{
+      path: "/",
+      method: RequestMethod.GET
+    }]
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist           : true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.PORT, () => {
+    logger.log(`Application is running on: http://localhost:${envs.PORT}`);
+  });
 }
 bootstrap();
