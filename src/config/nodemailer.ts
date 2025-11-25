f819e85968e638c299c0f9a8b426b4706c16150f
Rody Huancas <rodyhuancas.04@gmail.com>
2025-11-24 23:01:49 -0500

    Feat: Implement nodemailer transporter configuration

---
+++ src/config/nodemailer.ts
@@ -0,0 +1,16 @@
+import { SmtpConfig } from '@common/dtos';
+import * as nodemailer from 'nodemailer';
+
+export const transporter = (smtpConfig: SmtpConfig) => {
+  return nodemailer.createTransport({
+    host  : smtpConfig.host,
+    port  : smtpConfig.port,
+    secure: smtpConfig.secure || false,
+    auth  : smtpConfig
+      ? {
+          user: smtpConfig.auth?.user,
+          pass: smtpConfig.auth?.pass,
+        }
+      :  undefined,
+  });
+};
