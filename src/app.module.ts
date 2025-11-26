a2c783002d85422407dc36e55a803d28a6696c4f
Rody Huancas <rodyhuancas.04@gmail.com>
2025-11-25 22:46:29 -0500

    Feat: Import EmailModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { EmailModule } from './modules/email/email.module';
 
 @Module({
-  imports: [],
+  imports: [EmailModule],
 })
 export class AppModule {}
