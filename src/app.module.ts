60cd0b61fbd318848d7c08233290bfd76f21142a
Rody Huancas <rodyhuancas.04@gmail.com>
2025-12-09 19:45:09 -0500

    Feat: Add CronModule to AppModule for scheduled email processing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CronModule } from './modules/cron/cron.module';
 import { EmailModule } from './modules/email/email.module';
 
 @Module({
-  imports: [EmailModule],
+  imports: [EmailModule, CronModule],
 })
 export class AppModule {}
